54bb52ca7a5624e7d986191f15ef2fd13e42c3db
Abhilash Dash <abhilash200428@gmail.com>
2026-03-23 11:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+const list = document.getElementById("infi-list");
 
+let count = 1;
+
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    const li = document.createElement("li");
+    li.textContent = "Item " + count++;
+    list.appendChild(li);
+  }
+}
+
+addItems(10);
+
+list.addEventListener("scroll", function () {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2); 
+  }
+});
